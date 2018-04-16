f1469424a30434d78202254c48a9301dd6f8a831
Sebastian Garces <sgarcesc@gmail.com>
2018-04-16 06:39:05 -0500

    innitial commit

---
+++ Teller-01/Teller-01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teller_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
